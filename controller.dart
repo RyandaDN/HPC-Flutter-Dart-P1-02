cd9fb28619821468ce05d72de82f5d6a480d572f
RyandaDN <118288771+RyandaDN@users.noreply.github.com>
2022-11-16 23:18:20 +0700

    Create controller.dart

---
+++ controller.dart
@@ -0,0 +1,15 @@
+import 'dart:developer';
+
+import 'package:get/get.dart';
+
+class Controller extends GetxController {
+  var counter = 0.obs;
+
+  void add() {
+    counter++;
+  }
+
+  void minus() {
+    counter--;
+  }
+}
